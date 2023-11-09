485872a0ede5024c5664825a7573345e2d2707f2
yasuoOwO <heinmina05@gmail.com>
2023-11-09 09:03:21 +0630

    add admin panel folder & css file & folder structure prepare'

---
+++ src/main/java/com/ai/jwd42/controller/admin/AdminController.java
@@ -0,0 +1,24 @@
+package com.ai.jwd42.controller.admin;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ai.jwd42.dto.User;
+
+@Controller
+public class AdminController {
+	@RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+	public ModelAndView dashboard(HttpServletRequest request, ModelMap model) {
+		User user = new User();
+		return new ModelAndView("admin/home.html","user", user);
+	}
+	
+	
+
+	
+}
---
+++ src/main/java/com/ai/jwd42/controller/admin/BrandController.java
@@ -0,0 +1,19 @@
+package com.ai.jwd42.controller.admin;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ai.jwd42.dto.User;
+@Controller
+public class BrandController {
+	@RequestMapping(value = "/brand", method = RequestMethod.GET)
+	public ModelAndView brand(HttpServletRequest request, ModelMap model) {
+		User user = new User();
+		return new ModelAndView("admin/brand/index.html","user", user);
+	}
+}
